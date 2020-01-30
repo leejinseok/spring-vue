5b1a13ed186d908b9caeaf8f50b9afccc31363f5
이진석 <leejinseok@wemakeprice.com>
2020-01-30 18:25:16 +0900

    update

--- src/main/java/com/example/vue/domain/article/ArticleRepository.java
+++ src/main/java/com/example/vue/domain/article/ArticleRepository.java
@@ -1,4 +1,14 @@
 package com.example.vue.domain.article;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
 public class ArticleRepository {
+
+    private final EntityManager em;
+
 }
--- src/main/java/com/example/vue/domain/user/UserRepository.java
+++ src/main/java/com/example/vue/domain/user/UserRepository.java
@@ -1,7 +1,19 @@
 package com.example.vue.domain.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+
 @Repository
+@RequiredArgsConstructor
 public class UserRepository {
+
+    private final EntityManager em;
+
+    public User save(User user) {
+        em.persist(user);
+        return user;
+    }
+
 }
