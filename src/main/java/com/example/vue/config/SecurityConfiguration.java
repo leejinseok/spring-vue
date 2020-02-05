d03601d776f126895280621ca67625345062e3d4
이진석 <leejinseok@wemakeprice.com>
2020-02-05 14:40:42 +0900

    update

--- src/main/java/com/example/vue/config/SecurityConfiguration.java
+++ src/main/java/com/example/vue/config/SecurityConfiguration.java
@@ -1,15 +1,11 @@
 package com.example.vue.config;
 
 import com.example.vue.config.security.JwtAuthenticationFilter;
-import com.example.vue.config.security.UserDetailsAuthenticationProvider;
-import com.example.vue.domain.user.UserDetailsServiceImpl;
 import com.example.vue.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,13 +23,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Value("${jwt.secret}")
     private String secret;
-
-    private final UserDetailsServiceImpl userDetailService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -55,11 +44,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        return new UserDetailsAuthenticationProvider(userDetailService, bCryptPasswordEncoder());
     }
 
     @Bean
--- src/main/java/com/example/vue/domain/user/User.java
+++ src/main/java/com/example/vue/domain/user/User.java
@@ -4,15 +4,11 @@ import com.example.vue.domain.auth.RegisterRequestDto;
 import io.jsonwebtoken.Claims;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Collection;
 
 @Entity
 @Getter
